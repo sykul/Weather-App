1e58092d66a323aeeabd9b57f529e4bbe47da64c
sykul <luke.flatley@outlook.com>
2024-07-31 17:37:10 +0100

    create basic structure of divs for page layout

---
+++ script.js
@@ -0,0 +1,10 @@
+const colours = ["#003f5c","#58508d","#8a508f","#bc5090","#de5a79","#ff6361","#ff8531","#ffa600"]
+
+const createCard = function createTodaysWeatherCard() {
+
+}
+
+const createForecast = function createSevenDayForecastCard() {
+
+}
+
