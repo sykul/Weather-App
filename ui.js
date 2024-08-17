993d8c50aae5c4e00250a948b50541cd6d086aff
sykul <luke.flatley@outlook.com>
2024-08-17 13:59:46 +0200

    update today's temp

--- ui.js
+++ ui.js
@@ -1,14 +1,32 @@
-const updateToday = async function updateTodaysWeatherCard(promise) {
-  const location = await promise;
-  const locationName = location.address;
-  console.log("today's data:");
+const searchBox = document.querySelector(".search-box");
+const locationHeading = document.querySelector(".location-heading");
+const todayCardLeft = document.querySelector(".today-card-left");
+const todayCardRight = document.querySelector(".today-card-right");
+
+const clearSearchBox = function clearSearchBoxContent() {
+  searchBox.value = "";
+};
+
+const updateHeading = function updateLocationHeading(location) {
+  locationHeading.textContent = location.resolvedAddress;
+};
+
+const updateToday = async function updateTodaysWeatherCard(locationObject) {
+  const locationName = locationObject.address;
+  const today = locationObject.days[0];
+  const todaysWeather = today.icon;
+  todayCardLeft.textContent = todaysWeather;
+
+  todayCardRight.querySelector(".temp").textContent = today.temp;
+  locationObject.console.log("today's data:");
   console.log(locationName);
 };
 
-const updateForecast = async function updateSevenDayForecastCard(promise) {
-  const data = await promise;
+const updateForecast = async function updateSevenDayForecastCard(
+  locationObject
+) {
   console.log("forecast data:");
-  console.log(data);
+  console.log(locationObject);
 };
 
 const appendFave = function appendToFavouritesSidebar() {};
@@ -20,8 +38,11 @@ const clickOnFave = function callApiWhenFaveClicked() {};
 const displayLocationDropdown = function displayDropdownListOfLocations() {};
 
 const updateView = async function displayRetrievedWeatherDataOnPage(promise) {
-  updateToday(promise);
-  updateForecast(promise);
+  const locationObject = await promise;
+  clearSearchBox();
+  updateHeading(locationObject);
+  updateToday(locationObject);
+  updateForecast(locationObject);
 };
 
 const returnTopLocation = function returnTopLocationFromDropdownList() {};
