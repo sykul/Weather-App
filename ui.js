af686ac011378d795dc272791771a0a259a13bf2
sykul <luke.flatley@outlook.com>
2024-08-17 17:09:38 +0200

    show weather forecasts (no icons yet)

--- ui.js
+++ ui.js
@@ -2,6 +2,8 @@ const searchBox = document.querySelector(".search-box");
 const locationHeading = document.querySelector(".location-heading");
 const todayCardLeft = document.querySelector(".today-card-left");
 const todayCardRight = document.querySelector(".today-card-right");
+const forecastDays = document.querySelectorAll(".forecast-cards > div");
+console.log(forecastDays);
 
 const clearSearchBox = function clearSearchBoxContent() {
   searchBox.value = "";
@@ -17,16 +19,25 @@ const updateToday = async function updateTodaysWeatherCard(locationObject) {
   const todaysWeather = today.icon;
   todayCardLeft.textContent = todaysWeather;
 
-  todayCardRight.querySelector(".temp").textContent = today.temp;
-  locationObject.console.log("today's data:");
+  todayCardRight.querySelector(".temp").textContent = today.feelslike;
+  todayCardRight.querySelector(".max-temp").textContent = today.feelslikemax;
+  todayCardRight.querySelector(".min-temp").textContent = today.feelslikemin;
+  todayCardRight.querySelector(
+    ".rain-prob"
+  ).textContent = `${today.precipprob}%`;
+  todayCardRight.querySelector(".moon-phase").textContent = today.moonphase;
+
+  console.log("today's data:");
   console.log(locationName);
 };
 
 const updateForecast = async function updateSevenDayForecastCard(
   locationObject
 ) {
-  console.log("forecast data:");
-  console.log(locationObject);
+  forecastDays.forEach((day, index) => {
+    console.log(locationObject);
+    day.textContent = locationObject.days[index].icon;
+  });
 };
 
 const appendFave = function appendToFavouritesSidebar() {};
